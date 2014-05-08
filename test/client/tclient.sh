1e68659815e9ccc1e235150c654a59e3dc1e1e16
Vova <volodymyr.tymoshchuk@globallogic.com>
2014-05-08 13:34:16 -0700

    ffmpeg test

--- test/client/tclient.sh
+++ test/client/tclient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #../enc_client.py 127.0.0.0.1:8080 && 
-~/bin/ffmpeg -re -i ./crocodile_dundee.avi -t 00:10:00 -f flv -ar 11025 rtmps://127.0.0.1:8080/rtmp/test_vova?key=key.pem 
+~/bin/ffmpeg -re -i ./output.flv -t 00:00:03 -f flv -ar 11025 rtmps://127.0.0.1:8080/rtmp/test_vova?key=key.pem 
 
 
