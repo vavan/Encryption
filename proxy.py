e3f116c6c9954de031530f2d4849f12ffe162667
Vova <volodymyr.tymoshchuk@globallogic.com>
2014-05-20 19:40:31 -0700

    Prefix for CROSS build. Proxy clean up

--- proxy.py
+++ proxy.py
@@ -16,17 +16,6 @@ logging.basicConfig(level=logging.DEBUG,
 def log(msg):
     logging.error(msg)
 
-class RecordMixIn:
-    def __init__(self):
-        self.unsecure = None
-    def nice(self, data):
-        return  ":".join("{:02x}".format(ord(c)) for c in data)
-    def wsend(self, data):
-        logging.debug(">>>%s"%nice(data))
-    def wrecv(self, data):
-        logging.debug("<<<%s"%nice(data))
-
-
 
 class Pipe(threading.Thread):
     BUFFER_SIZE = 10000
@@ -100,43 +89,20 @@ class Client(Pipe):
         self.s.connect((self.ip, int(self.port)))
         log("Client connected")
 
-class Socket2(socket.socket):
-    def __init__(self, other):
-        fd = dup(other.fileno())
-        socket.socket.__init__(self, other.family, other.type, other.proto, fileno=fd)
-        self.settimeout(other.gettimeout())
-    def recv(self, buf, flags = 0):
-        log("!!!!!!!!!!!!!")
-        return super.recv(buf, flags)
 
 class Server(Pipe):
     def __init__(self, parent, secure, socket):
         Pipe.__init__(self, parent, secure, socket)
     def create(self):
         if self.secure:
-            z = Socket2(self.s)
-            self.s = ssl.wrap_socket(z,
+            self.s = ssl.wrap_socket(self.s,
                                server_side=True,
                                certfile="cert.pem",
-                               keyfile="cert.pem",
+                               keyfile="key.pem",
                                ssl_version=ssl.PROTOCOL_SSLv23)
         log("Server connected")
 
-class DumpServer(Server, RecordMixIn):
-    def __init__(self, parent, secure, socket):
-        Server.__init__(self, parent, secure, socket)
-        RecordMixIn.__init__(self)
-        self.unsecure = self.s
-    def recv(self):
-        data = self.unsecure.recv(Pipe.BUFFER_SIZE)
-        log("Unsecured Pipe received %s bytes"%len(data))
-        self.wrecv(data)
-##        if data:
-##            self.other.send(data)
-        return data
 
-class KeyMngr:
-    pass
 
 class Listener:
     def __init__(self, server_url, client_url, Server_Class):
@@ -186,12 +152,8 @@ def parse_url(url):
 if len(sys.argv) >= 3:
     server = sys.argv[1]
     client = sys.argv[2]
-    if len(sys.argv) == 4:
-        Server_Class = DumpServer
-    else:
-        Server_Class = Server
 
-    ps = Listener(parse_url(server), parse_url(client), Server_Class)
+    ps = Listener(parse_url(server), parse_url(client), Server)
     ps.start()
 else:
     print("USAGE: [s]serverip:port [s]clientip:port [dump]")
